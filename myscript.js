f9086eb4b990e8adff5492db0a90d58934910055
MatteoSanson <matteo.sanson@gmail.com>
2023-10-31 16:09:30 +0100

    feat: update password e console

--- myscript.js
+++ myscript.js
@@ -2,17 +2,20 @@
 
 
 const nomeUtente = prompt ('Inserisci il tuo nome');
-console.log (nomeUtente);
+console.log ('Nome:', nomeUtente);
 const cognomeUtente = prompt ('Inserisci il tuo cognome');
-console.log (cognomeUtente);
+console.log ('Cognome:', cognomeUtente);
 const coloreUtente = prompt ('Inserisci il tuo colore preferito');
-console.log (coloreUtente);
+console.log ('Colore Preferito:', coloreUtente);
 
 let numeroCasuale = Math.floor(Math.random() * 101);
-console.log(numeroCasuale);
+console.log('Numero Casuale:', numeroCasuale);
+
+const password = nomeUtente + cognomeUtente + coloreUtente + numeroCasuale;
+console.log ('password generata:', password);
 
 generata.innerHTML = nomeUtente + cognomeUtente + coloreUtente + numeroCasuale;
 
-console.log (generata);
+
 
 
