2f7b41323799928d0a47f0248087d8661da4ec8e
MatteoSanson <matteo.sanson@gmail.com>
2023-10-31 15:52:44 +0100

    feat: generatore password con numero casuale

---
+++ myscript.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+const nomeUtente = prompt ('Inserisci il tuo nome');
+console.log (nomeUtente);
+const cognomeUtente = prompt ('Inserisci il tuo cognome');
+console.log (cognomeUtente);
+const coloreUtente = prompt ('Inserisci il tuo colore preferito');
+console.log (coloreUtente);
+
+let numeroCasuale = Math.floor(Math.random() * 101);
+console.log(numeroCasuale);
+
+generata.innerHTML = nomeUtente + cognomeUtente + coloreUtente + numeroCasuale;
+
+
+// numero casuale
+
